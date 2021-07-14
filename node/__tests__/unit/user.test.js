65011703c6623268f7ab079b8edaf7cb3c97d02b
FelipeCararo <felipe.gorges.cararo@gmail.com>
2021-07-14 07:59:09 -0300

    Remove lines test

--- node/__tests__/unit/user.test.js
+++ node/__tests__/unit/user.test.js
@@ -2,7 +2,6 @@ require('../../src/database/');
 const bcrypt = require('bcryptjs');
 
 const User = require('../../src/app/models/User');
-const Citie = require('../../src/app/models/Citie');
 
 describe('User tests', () => {
   beforeEach(async () => {
@@ -15,7 +14,6 @@ describe('User tests', () => {
       email: 'felipe@gmail.com',
       password: '123456',
     });
-    const teste = await Citie.findByPk(1);
 
     const compareHash = await bcrypt.compare('123456', usercreate.password);
 
