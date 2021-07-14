7ca41b2576a3f96b224eef40fe0936bbf201d87b
FelipeCararo <felipe.gorges.cararo@gmail.com>
2021-07-13 23:50:37 -0300

    Ajust comments

--- node/__tests__/factories.js
+++ node/__tests__/factories.js
@@ -1,26 +1,11 @@
 const faker = require('faker');
 const { factory } = require('factory-girl');
 const User = require('../src/app/models/User');
-// const Citie = require('../src/app/models/Citie');
-// const Cliente = require('../src/app/models/Cliente');
 
 factory.define('User', User, {
   name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
 });
-
-// factory.define('Citie', Citie, {
-//   name: faker.address.country(),
-//   state: faker.address.state(),
-// });
-
-// factory.define('Cliente', Cliente, {
-//   name: faker.name.findName(),
-//   sex: 'male',
-//   birthdate: '199-08-16',
-//   age: 26,
-//   cit_id: 1,
-// });
 
 module.exports = factory;
